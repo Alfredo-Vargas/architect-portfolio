73c68b293bd759bc9c56469a585ccc8a9471f68f
Alfredo Vargas <alfredo.vargas-paredes@outlook.com>
2021-06-16 18:53:51 +0200

    design page started

--- architectPortfolio/js/general.js
+++ architectPortfolio/js/general.js
@@ -1,5 +1,60 @@
 var menu_on = false;
+var img_sources = ["/proj_img/bancodado0.jpg", "/proj_img/comoda0.jpg", "/proj_img/mesa0.jpg", "/proj_img/SofaMesa0.jpg", "/proj_img/puff_shelf0.jpg"];
+var img_descriptions = ["Dice Stools", "Chest of Dressing", "Center-Side Table", "Sofa-Table", "Puff-Shelf"];
+var totalSlides = 4; // starts counting from 0
+var slideIndex = 0;
 
+var touchstartX = 0, touchstartY = 0;
+var touchendX = 0, touchendY = 0;
+
+window.onload = function(){
+    displaySlide(slideIndex); 
+
+    var gesuredZone = document.getElementById("slides_container");
+    gesuredZone.addEventListener('touchstart', function(event) {
+        touchstartX = event.touches[0].clientX;
+        touchstartY = event.touches[0].clientY;
+    }, false);
+    gesuredZone.addEventListener('touchend', function(event) {
+        touchendX = event.changedTouches[0].clientX;
+        touchendY = event.changedTouches[0].clientY;
+        handleGesure();
+    }, false);
+}
+
+function displaySlide(slideIndex){
+    let image = document.getElementById("active_image");
+    let descr = document.getElementById("img_description");
+    image.src = img_sources[slideIndex];
+    descr.innerHTML = img_descriptions[slideIndex];
+}
+
+function handleGesure() {
+    var swiped = 'swiped: ';
+    if (touchendX < touchstartX) {
+        if (slideIndex < totalSlides){
+            slideIndex++;
+            displaySlide(slideIndex);
+        }
+    }
+    if (touchendX > touchstartX) {
+        if (slideIndex > 0){
+            slideIndex--;
+            displaySlide(slideIndex);
+        }
+    }
+    /*
+    if (touchendY < touchstartY) {
+        alert(swiped + 'down!');
+    }
+    if (touchendY > touchstartY) {
+        alert(swiped + 'left!');
+    }
+    if (touchendY == touchstartY) {
+        alert('tap!');
+    }
+    */
+}
 function toggleMenu(){
     menu_on = !menu_on;
     let wrapper = document.getElementsByClassName("wrapper")[0];
